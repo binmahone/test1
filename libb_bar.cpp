d2b2f773c6e944a38d0b53ee8e36d9dbdd9a023b
Hongbin Ma <mahongbin@apache.org>
2023-03-23 22:58:50 +0800

    liblibb with jemalloc , malloc symbol undfined?

--- libb_bar.cpp
+++ libb_bar.cpp
@@ -1,9 +1,25 @@
 //
 // Created by hongbin on 23-3-20.
 //
-int libb_bar_v = 1;
-int xxx = 10;
 
-int yyy() {
-    return 10;
+#include <iostream>
+#include <cstring>
+
+int liba_v_foo;
+extern int yyy();
+
+int func() {
+    int mm = yyy();
+    void *  x= malloc(100000000);
+    memset(x,1,100000000);
+    long sum = 0;
+    int* p = static_cast<int*>(x);
+    for(int i=0;i<100000000/sizeof(int);i++)
+    {
+        sum+=p[i];
+    }
+    std::cout << "in libb_bar:"<<sum << std::endl;
+    free(x);
+
+    return sum;
 }
--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
-//#include <boost/filesystem.hpp>
 #include <iostream>
-#include "liba_foo.h"
-#include <cstdlib>
-
-//#include <glog/logging.h>
-//using namespace boost::filesystem;
-
 #include <cstring>
-#include <malloc.h>
 
-extern int xxx;
-
-
-int yyy() {
-    return 90;
-}
+extern int func();
+extern int yyy();
 
 int main()
 {
-    std::cout << "yyy is : " << yyy() << std::endl;
     void *  x= malloc(100000000);
     memset(x,1,100000000);
     long sum = 0;
@@ -28,14 +15,12 @@ int main()
         sum+=p[i];
     }
     std::cout << sum << std::endl;
+
+
+    func();
+
     free(x);
 
-    struct mallinfo minfo = mallinfo();
-    printf("Total number of bytes allocated: %d\n", minfo.uordblks);
-    printf("Number of chunks allocated: %d\n", minfo.ordblks);
-    printf("Number of chunks in use: %d\n", minfo.hblks);
-    printf("Number of bytes in use: %d\n", minfo.arena);
-    return 0;
 
 //    path p{"/tmp/cachelib"};
 //    std::cout << p.generic_string() << '\n';
